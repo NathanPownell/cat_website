bf729cb10d35e6446d903b6ae53023cb1f134fea
Nathaniel Pownell <107416244+NathanPownell@users.noreply.github.com>
2024-04-29 17:12:33 -0400

    Add mobile nav top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,15 @@ import Content from "@/components/content";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import MainInfoCard from "@/components/main-info-card"
+import Image from "next/image";
 
 export default function Home() {
 
   return (
+    <>
+        <div className="z-20 sticky top-0 bg-background shadow-md w-full flex flex-col p-2 items-center md:hidden">
+          <Image src='/bonshatiroshow.png' alt='bon shapiro show' width={100} height={80} />
+        </div>
     <main className="flex min-h-screen min-w-screen flex-col items-center gap-12">
       <div className="z-10 flex flex-col w-full gap-12 items-center justify-center  text-sm lg:flex">
         <Hero />
@@ -23,5 +28,6 @@ export default function Home() {
         </Container>
       </div>
     </main>
+    </>
   );
 }
