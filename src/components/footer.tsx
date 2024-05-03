0f5930ca2c7b82e087b87b6b49971f46f08fca9a
Nathaniel Pownell <107416244+NathanPownell@users.noreply.github.com>
2024-05-03 16:37:28 -0400

    Center footer gif

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='w-full flex flex-col content-center text-center p-4 bg-muted'>
+    <div className='w-full flex flex-col items-center text-center p-4 bg-muted'>
               <Image src="/bon.gif" alt="bon" width={500} height={500} />
         <h2 className='text-2xl font-bold'>Facts, b*tch.</h2>
         <p className='text-sm'>Not affiliated with Ben Shapiro or the Daily Wire.</p>
