b0118a43ec6d0980b41c6bbfbe8d5a828e32b7fc
Nathaniel Pownell <107416244+NathanPownell@users.noreply.github.com>
2024-12-14 23:31:37 -0500

    ca

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "TBD";
+export const CONTRACT_ADDRESS = "DxKYbiyJrPQ1s7YW4JDJxEfppsncsDeCD4SZqQ5Epump";
 export const TICKER = "$FRTCAT";
 export const TWITTER_LINK = "https://x.com/frtcatsolana";
 export const TELEGRAM_LINK = "https://t.me/FRTCAT";
